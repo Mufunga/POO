9887627b8530341304d480ba7069b07d45018c01
guelord Mufunga <gmufunga76@hotmail.com>
2023-09-17 20:43:35 +0200

    Ajout de la methose save et all player

--- views/view_player.py
+++ views/view_player.py
@@ -5,37 +5,24 @@ class View_payer:
 
         print("Enter your informations".center(80,"*") )
 
-        player_info["first_name"] = input("Enter your First_Name: \n")
-        player_info["family_name"] = input("Family Name:\n")
-        player_info["birthday"] = input("Birthday:\n ")
+        player_info["first_name"] = input("Enter your first_Name: \n")
+        player_info["name"] = input("name:\n")
+        player_info["birthday"] = input("birthday:\n ")
         player_info["chess_id"] = input("chess_id:\n")
 
         return player_info
     
     def desplay_player_menu(self):
         while True:
-            print("\n", "Welcome to the Chess game".center(80,"-"), "\n")
+            print("\n", "Players manager".center(80,"-"), "\n")
             print("choose the following choice : ")
-            print("1.Tournemant manager")
-            print("2.Player manager")
-            print("3.Exit")
+            print("1.create player")
+            print("2.show players list")
+            print("3.Return to the previous menu")
+
             choice = input("\nEnter your choice : ")
-            if choice == "1":
-                print("Tournement Manager : \n")
-
-            elif choice == "2":
-
-                print("1.Create player")
-                print("2.Show players List by name order ")
-                choice = input("\nEnter your choice : ")
-                if choice == "1":
-                    player_infos = self.get_info_player()
-                    return player_infos
-                    #print(player["first_name"])
-                elif choice == "2":
-                    print("Show players List by name order ")
-            elif choice == "3":
-                return "Merci pour votre visite"
+            if choice in ["1","2","3"]:
+               return choice
             else:
                 print("invalid choice\n")
             
@@ -45,4 +32,4 @@ class View_payer:
 
 if __name__=="__main__" :
     view_player = View_payer()
-    print(view_player.desplay_player_menu())
+    print(view_player.get_info_player())
